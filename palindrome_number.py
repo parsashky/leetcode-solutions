9aa39c04a739c9f731faa88881cd6a2a7c074a80
parsa <mohammadrezaparsaii@gmail.com>
2025-11-19 15:21:51 +0330

    add palindorme number

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,5 +1,8 @@
-def ispalindrome(x):
-    while x % 10 == 0 :
-        a ==  x
-        y = x % 10
-        z = a -(y * 10)
+def ispalindrome(x):       
+    y = x[::-1]  
+    if x == y:
+        return f"{x} is palindrome number."
+    else:
+        return f"{x} is not palindrome number."
+number = input("enter your number :")
+print(ispalindrome(number))
