05ce5e8e65ef8f3482f53006ea0330cb68792213
parsa <mohammadrezaparsaii@gmail.com>
2025-11-24 21:08:53 +0330

    add plus one

---
+++ plus_one.py
@@ -0,0 +1,19 @@
+def plus_one():
+    digits = [9,9]
+    digits.reverse()
+    
+    digits[0] += 1  
+
+    b = 0
+    for i in range(len(digits)):
+        if digits[i] >= 10:
+            a = (digits[i] % 10) + b
+            digits[i] = a
+            b = 1
+    if b == 1:
+        digits.append(1)
+
+    digits.reverse()
+    return digits
+
+print(plus_one())
